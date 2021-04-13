eefc49c18b06f4e2934ae07c8d0d8982db03f3bc
GaboBastardo30 <gabitobastardo@gmail.com>
2021-04-13 09:04:34 -0430

    Aplicacion de IMC en Visual C#

---
+++ IndiceMasaCorporalApp/IndiceMasaCorporalApp/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USUARIO
+ * Date: 10/04/2021
+ * Time: 12:57 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace IndiceMasaCorporalApp
+{
+	partial class Prueba
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtAltura = new System.Windows.Forms.TextBox();
+			this.txtResultado = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.btnLimpiar = new System.Windows.Forms.Button();
+			this.txtPeso = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCalcular = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(65, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(103, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "IMC App";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(52, 107);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(139, 23);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Altura en Centimetros:";
+			// 
+			// txtAltura
+			// 
+			this.txtAltura.Location = new System.Drawing.Point(85, 133);
+			this.txtAltura.Name = "txtAltura";
+			this.txtAltura.Size = new System.Drawing.Size(67, 20);
+			this.txtAltura.TabIndex = 1;
+			// 
+			// txtResultado
+			// 
+			this.txtResultado.Font = new System.Drawing.Font("Engravers MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtResultado.Location = new System.Drawing.Point(95, 263);
+			this.txtResultado.Name = "txtResultado";
+			this.txtResultado.Size = new System.Drawing.Size(44, 32);
+			this.txtResultado.TabIndex = 7;
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(65, 227);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 22);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Resultado:";
+			// 
+			// btnLimpiar
+			// 
+			this.btnLimpiar.Location = new System.Drawing.Point(65, 311);
+			this.btnLimpiar.Name = "btnLimpiar";
+			this.btnLimpiar.Size = new System.Drawing.Size(100, 27);
+			this.btnLimpiar.TabIndex = 3;
+			this.btnLimpiar.Text = "Limpiar Resultado";
+			this.btnLimpiar.UseVisualStyleBackColor = true;
+			this.btnLimpiar.Click += new System.EventHandler(this.BtnLimpiarClick);
+			// 
+			// txtPeso
+			// 
+			this.txtPeso.Location = new System.Drawing.Point(85, 84);
+			this.txtPeso.Name = "txtPeso";
+			this.txtPeso.Size = new System.Drawing.Size(66, 20);
+			this.txtPeso.TabIndex = 0;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(52, 58);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(139, 23);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Peso en Kilogramos:";
+			// 
+			// btnCalcular
+			// 
+			this.btnCalcular.Location = new System.Drawing.Point(65, 171);
+			this.btnCalcular.Name = "btnCalcular";
+			this.btnCalcular.Size = new System.Drawing.Size(100, 27);
+			this.btnCalcular.TabIndex = 12;
+			this.btnCalcular.Text = "Hacer Calculo";
+			this.btnCalcular.UseVisualStyleBackColor = true;
+			this.btnCalcular.Click += new System.EventHandler(this.BtnCalcularClick);
+			// 
+			// Prueba
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(223, 361);
+			this.Controls.Add(this.btnCalcular);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtPeso);
+			this.Controls.Add(this.btnLimpiar);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.txtResultado);
+			this.Controls.Add(this.txtAltura);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label1);
+			this.MaximumSize = new System.Drawing.Size(235, 395);
+			this.MinimumSize = new System.Drawing.Size(235, 395);
+			this.Name = "Prueba";
+			this.Text = "PruebaWindowsForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button btnLimpiar;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtResultado;
+		private System.Windows.Forms.TextBox txtAltura;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtPeso;
+		private System.Windows.Forms.Button btnCalcular;
+	}
+}
---
+++ IndiceMasaCorporalApp/IndiceMasaCorporalApp/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IndiceMasaCorporalApp
+{
+
+	public partial class Prueba : Form
+	{
+
+		
+		public Prueba()
+		{
+			InitializeComponent();
+		}
+		
+		void BtnCalcularClick(object sender, EventArgs e)
+		{
+			//LAS VARIABLES
+			
+			int peso;
+		    Double altura;
+		    Double imc;
+		    Double imc2;
+		    
+		    //LOS CALCULOS
+			
+		    peso = Convert.ToInt32(txtPeso.Text);
+		    altura = Convert.ToInt32(txtAltura.Text);
+			
+			Double.TryParse(txtAltura.Text, out altura);
+			Int32.TryParse(txtPeso.Text, out peso);
+			
+			imc = Convert.ToDouble( peso/ (altura/100 * altura/100));
+			imc2 = Convert.ToInt16(imc);
+			
+			//MOSTRANDO EL CALCULO EN LA TEXTBOX
+			
+			txtResultado.Text = imc2.ToString();
+		}
+		
+		
+		void BtnLimpiarClick(object sender, EventArgs e)
+		{
+			//LIMPIANDO LOS TEXTBOXS Y HACIENDO FOCUS EN EL TEXTBOX DE ALTURA
+			
+			txtPeso.Clear();
+			txtAltura.Clear();
+			txtResultado.Clear();
+			txtPeso.Focus();
+		}
+	}
+}
